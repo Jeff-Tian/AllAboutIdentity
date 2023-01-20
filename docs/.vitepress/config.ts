01890e93c16eac9887a20aebd44ba7e087d047ba
Jeff Tian <jeff.tian@outlook.com>
2023-01-20 14:03:09 +0800

    feat(UNI-110): add mindmap

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vitepress';
+import {defineConfig, MarkdownOptions} from 'vitepress';
+import MermaidExample from "./mermaid-markdown-all";
+
+const allMarkdownTransformers: MarkdownOptions = {
+  // the shiki theme to highlight code blocks
+  theme: 'github-dark',
+  config: async (md) => {
+    await MermaidExample(md);
+  },
+};
 
 // refer https://vitepress.vuejs.org/config/introduction for details
 export default defineConfig({
   lang: 'en-US',
   title: 'VitePress',
   description: 'Vite & Vue powered static site generator.',
+  markdown: allMarkdownTransformers,
 
   themeConfig: {
     nav: [
       { text: 'Example', link: '/example' },
+      { text: 'Idea', link: '/idea' },
 
       // {
       //   text: 'Dropdown Menu',
@@ -27,6 +38,7 @@ export default defineConfig({
         // text: 'Guide',
         items: [
           { text: 'Example', link: '/example' },
+          { text: 'Idea', link: '/idea' },
           // ...
         ],
       },
---
+++ docs/idea.md
@@ -0,0 +1,24 @@
+# Idea
+
+## Mindmap
+
+```mermaid
+mindmap
+  root((身份认证))
+    OAuth 2.0
+      授权码许可
+      ::icon(fa fa-book)
+      客户端许可
+      隐式许可
+      设备码许可
+    OIDC
+      
+    IdP
+      authing.cn
+      Keycloak
+      Duende IdentityServer
+      Okta
+      Auth 0
+      Azure AD
+      ...
+```
