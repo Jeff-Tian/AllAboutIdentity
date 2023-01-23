e1860a3270adfd59b4ffce624ad4b0ec481d5c4d
Jeff Tian@Workstation <jie.tian@live.cn>
2023-01-23 22:22:10 +0800

    feat(UNI-115): nav

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,46 +2,52 @@ import {defineConfig, MarkdownOptions} from 'vitepress';
 import MermaidExample from "./mermaid-markdown-all";
 
 const allMarkdownTransformers: MarkdownOptions = {
-  // the shiki theme to highlight code blocks
-  theme: 'github-dark',
-  config: async (md) => {
-    await MermaidExample(md);
-  },
+    // the shiki theme to highlight code blocks
+    theme: 'github-dark',
+    config: async (md) => {
+        await MermaidExample(md);
+    },
 };
 
 // refer https://vitepress.vuejs.org/config/introduction for details
 export default defineConfig({
-  lang: 'en-US',
-  title: 'VitePress',
-  description: 'Vite & Vue powered static site generator.',
-  markdown: allMarkdownTransformers,
+    lang: 'en-US',
+    title: 'VitePress',
+    description: 'Vite & Vue powered static site generator.',
+    markdown: allMarkdownTransformers,
 
-  themeConfig: {
-    nav: [
-      { text: 'Example', link: '/example' },
-      { text: 'Idea', link: '/idea' },
+    themeConfig: {
+        nav: [
+            {text: 'Example', link: '/example'},
+            {text: 'Preface', link: '/preface'},
+            {text: 'Concepts', link: '/concepts'},
+            {text: 'Applications', link: '/applications/BFF'},
+            {text: 'Idea', link: '/idea'},
 
-      // {
-      //   text: 'Dropdown Menu',
-      //   items: [
-      //     { text: 'Item A', link: '/item-1' },
-      //     { text: 'Item B', link: '/item-2' },
-      //     { text: 'Item C', link: '/item-3' },
-      //   ],
-      // },
+            // {
+            //   text: 'Dropdown Menu',
+            //   items: [
+            //     { text: 'Item A', link: '/item-1' },
+            //     { text: 'Item B', link: '/item-2' },
+            //     { text: 'Item C', link: '/item-3' },
+            //   ],
+            // },
 
-      // ...
-    ],
-
-    sidebar: [
-      {
-        // text: 'Guide',
-        items: [
-          { text: 'Example', link: '/example' },
-          { text: 'Idea', link: '/idea' },
-          // ...
+            // ...
         ],
-      },
-    ],
-  },
+
+        sidebar: [
+            {
+                // text: 'Guide',
+                items: [
+                    {text: 'Example', link: '/example'},
+                    {text: 'Preface', link: '/preface'},
+                    {text: 'Concepts', link: '/concepts'},
+                    {text: 'Applications', link: '/applications/BFF'},
+                    {text: 'Idea', link: '/idea'},
+                    // ...
+                ],
+            },
+        ],
+    },
 });
